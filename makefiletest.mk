ce7e7080170b2de78f09f057123e7584d5894120
Karakatka46 <56833208+Karakatka46@users.noreply.github.com>
2023-12-10 15:00:37 +0300

    Add files via upload

--- makefiletest.mk
+++ makefiletest.mk
@@ -3,22 +3,25 @@ SV_SOURCES = scr1_pipe_ialu.sv  # Укажите здесь все ваши фа
 TOP_MODULE = scr1_pipe_ialu
 COCOTB_SOURCES = $(shell cocotb-config --makefiles)/simulators/Makefile.inc
 SIM_BINARY = makefiletest
+COVERAGE = coverege 
 
 # Цель по умолчанию
 all: sim
-
-# Компиляция SystemVerilog и тестбенча
+coverage: $(COVERAGE)
 sim: $(SIM_BINARY)
 
-$(SIM_BINARY): $(SV_SOURCES) test.py $(COCOTB_SOURCES)
-	verilator --sv --trace -Wno-fatal --top-module $(TOP_MODULE) \
-			--Mdir obj_dir -y $(shell cocotb-config --prefix)/cocotb/libs -LDFLAGS "-ldl" \
-			$(SV_SOURCES)
+	$(SIM_BINARY): $(SV_SOURCES) test.py $(COCOTB_SOURCES)
+		verilator --sv --trace -Wno-fatal --top-module $(scr1_pipe_ialu) \
+				--Mdir obj_dir -y $(shell cocotb-config --prefix)/cocotb/libs -LDFLAGS "-ldl" \
+				$(SV_SOURCES)
+	$(COVERAGE): scr1_pipe_ialu.sv cocotb_test/test11.py
+    verilator -sv --coverage -I./cocotb/share/ -y./cocotb/share/ --Mdir $(COVERAGE) --cc $< cocotb_test/test11.py --exe sim_main.cpp
+    make -C $(COVERAGE) -f Vscr1_pipe_ialu.mk
 
-	make -C obj_dir -f V$(TOP_MODULE).mk
+	make -C obj_dir -f V$(scr1_pipe_ialu).mk
 # Запуск тестбенча
 run: sim
-	PYTHONPATH=$(shell cocotb-config --prefix)/cocotb/libs/ LD_LIBRARY_PATH=$(shell cocotb-config --prefix)/cocotb/libs/ ./obj_dir/V$(TOP_MODULE) -sv
+	PYTHONPATH=$(shell cocotb-config --prefix)/cocotb/libs/ LD_LIBRARY_PATH=$(shell cocotb-config --prefix)/cocotb/libs/ ./obj_dir/V$(scr1_pipe_ialu) -sv
 
 # Очистка временных файлов
 clean:
